4908fb6a26f85a2f7e459e668664ffbae7d020f4
Daniel.Hill <daniel.hill@entec-int.com>
2015-11-10 12:01:36 +0000

    test

--- bin/Android/Debug/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\S-HILL-M\Documents\externalwarehouseapp\ExternalWarehouseApp\ExternalWarehouseApp\bld\Debug\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\S-HILL-M\Documents\externalwarehouseapp\ExternalWarehouseApp\ExternalWarehouseApp\bld\Debug\platforms\android\AndroidManifest.xml \
--- bin/Android/Debug/MainActivity-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\S-HILL-M\Documents\externalwarehouseapp\ExternalWarehouseApp\ExternalWarehouseApp\bld\Debug\platforms\android\ant-build\MainActivity-debug-unaligned.apk : \
-C:\Users\S-HILL-M\Documents\externalwarehouseapp\ExternalWarehouseApp\ExternalWarehouseApp\bld\Debug\platforms\android\ant-build\MainActivity.ap_ \
-C:\Users\S-HILL-M\Documents\externalwarehouseapp\ExternalWarehouseApp\ExternalWarehouseApp\bld\Debug\platforms\android\ant-build\classes.dex \
-C:\Users\S-HILL-M\Documents\externalwarehouseapp\ExternalWarehouseApp\ExternalWarehouseApp\bld\Debug\platforms\android\CordovaLib\ant-build\classes.jar \
