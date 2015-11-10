cd7168aedff993b3510a4638eb7f8fec01131a59
Daniel.Hill <daniel.hill@entec-int.com>
2015-11-10 14:14:29 +0000

    test

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,7 +1,7 @@
-﻿var excelData = [];
+﻿
 
 function fetchData() {
-
+var excelData = [];
     $.post("http://192.168.254.64:2056/ExternalWarehouseAppHandler.ashx/opendocument?dt=" + (new Date().getTime()), function (json) {
         console.log("worked");
         excelData = ParseServerJSONString(json).Data;
